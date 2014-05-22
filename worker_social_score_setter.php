dccf6d40a995e8cd301edb81176951c1f567ad88
Mustapha <mustapha.hamoui@gmail.com>
2014-05-22 22:30:18 +0000

    added a social score class and a social score worker for cron jobs

---
+++ worker_social_score_setter.php
@@ -0,0 +1,56 @@
+<?php 
+
+/************************************************************************************************
+This script gets the social scores of all the posts in the last 7 days 
+and includes them in the posts database 
+************************************************************************************************/ 
+
+include_once("init.php");
+require_once("classes/SocialScore.class.php");
+require_once("classes/simplepie.php");
+
+// Define CLI Interface Elements
+$line_length = 70;
+$hr  = "\n".str_repeat('-', $line_length)."\n";
+$dhr = "\n".str_repeat('=', $line_length)."\n";
+
+$hours = 24 * 60;
+
+// If an argument is supplied, use its value as the number of hours;
+if ((!empty($argv[1])) && ($argv[1] > 0)) {
+  $hours = $argv[1];
+}
+
+
+$hoursAgo = time() - ($hours * 60 * 60);
+
+$posts = DB::getInstance()->query('SELECT post_url, post_visits FROM posts WHERE `post_timestamp` > '.$hoursAgo)->results();
+
+foreach($posts as $key => $post){
+  echo $post->post_url;
+  $postVisits = $post->post_visits;
+	$score = new SocialScore($post->post_url);
+  $totalScore = $score->getFacebookScore() + $score->getTwitterScore();
+  
+  // The formula for social score scales downward the social sharing to avoid disproportional figures;
+  $socialScore = $postVisits + round(sqrt($totalScore));
+
+  DB::getInstance()->query('UPDATE `posts` SET `post_facebookShares` = ' . $score->getFacebookScore() . ' WHERE `post_url` = "' . $post->post_url . '"');
+  DB::getInstance()->query('UPDATE `posts` SET `post_twitterShares` = ' . $score->getTwitterScore() . ' WHERE `post_url` = "' . $post->post_url . '"');
+  DB::getInstance()->query('UPDATE `posts` SET `post_totalShares` = ' . $totalScore . ' WHERE `post_url` = "' . $post->post_url . '"');
+	DB::getInstance()->query('UPDATE `posts` SET `post_socialScore` = ' . $socialScore . ' WHERE `post_url` = "' . $post->post_url . '"');
+  echo "\n";
+}
+
+//$posts = DB::query('SELECT * FROM posts WHERE ')
+
+// get all the posts in the last 12 hours
+// loop through these posts
+// for each of them
+//  get facebook score
+//  get twitter score
+//  store them
+//    example code: $query = 'UPDATE posts SET post_visits = post_visits+1 WHERE post_url = "'.$url.'"';
+//                  DB::getInstance()->query($query);]
+
+?>
