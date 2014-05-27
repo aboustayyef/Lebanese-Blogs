02ee1973d6e9f7e93f0265bce29363faf5f2e7ae
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2014-05-27 15:55:11 +0000

    modified social score to reduce influence of virality and issue a listicle penalty

--- worker_social_score_setter.php
+++ worker_social_score_setter.php
@@ -24,13 +24,13 @@ if ((!empty($argv[1])) && ($argv[1] > 0)) {
 
 $hoursAgo = time() - ($hours * 60 * 60);
 
-$posts = DB::getInstance()->query('SELECT post_url, post_visits FROM posts WHERE `post_timestamp` > '.$hoursAgo)->results();
+$posts = DB::getInstance()->query('SELECT post_title, post_url, post_visits FROM posts WHERE `post_timestamp` > '.$hoursAgo)->results();
 
 foreach($posts as $key => $post){
   echo $post->post_url;
   $postVisits = $post->post_visits;
 	$score = new SocialScore($post->post_url);
-
+  $title = $post->post_title;
   $totalScore = $score->getFacebookScore() + $score->getTwitterScore();
   
   // formula#1: $virality = 2+round(3 * sqrt($shares));
@@ -40,9 +40,20 @@ foreach($posts as $key => $post){
       $virality = 50;
   }
 
-  // The formula for social score scales downward the social sharing to avoid disproportional figures;
-  $socialScore = $postVisits + $virality;
+  // The formula for social score 
+  $socialScore = $postVisits + round($virality / 2);
+
+  // Listicle Penalty (20%)
+  if (isListicle($title)) {
+    $socialScore = round($socialScore*0.8);
+  }
+
+  // logging
   echo "\ntotal shares: $totalScore, Virality: $virality\n";
+  if (isListicle($title)) {
+    echo "\n(Listicle penalty added)\n";
+  }
+
   DB::getInstance()->query('UPDATE `posts` SET `post_facebookShares` = ' . $score->getFacebookScore() . ' WHERE `post_url` = "' . $post->post_url . '"');
   DB::getInstance()->query('UPDATE `posts` SET `post_twitterShares` = ' . $score->getTwitterScore() . ' WHERE `post_url` = "' . $post->post_url . '"');
   DB::getInstance()->query('UPDATE `posts` SET `post_totalShares` = ' . $totalScore . ' WHERE `post_url` = "' . $post->post_url . '"');
@@ -50,6 +61,18 @@ foreach($posts as $key => $post){
   DB::getInstance()->query('UPDATE `posts` SET `post_virality` = ' . $virality . ' WHERE `post_url` = "' . $post->post_url . '"');
   echo "\n";
 }
-// Note: $virality = round(2 + 1.5*(sqrt($totalScore)));
+
+function isListicle($title){
+  $title = strtolower($title); 
+  $parts = explode(" ", $title);
+  $firstWord = $parts[0];
+  $secondWord = $parts[1];
+  $listOfNumbers = array('3','4','5','6','7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', 'three','four','five','six','seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen');
+  if ((in_array($firstWord, $listOfNumbers))||(in_array($secondWord, $listOfNumbers))) {
+    return TRUE;
+  }else{
+    return FALSE;
+  }
+}
 
 ?>
